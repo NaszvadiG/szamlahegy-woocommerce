6e4885f830cebc4c4b25f5a1e35b5c08faba677a
kepes.peter <kepes.peter@codeplay.hu>
2016-09-26 14:06:18 +0200

    version number changed to 1.1.2

--- szamlahegy-woocommerce.php
+++ szamlahegy-woocommerce.php
@@ -11,7 +11,7 @@
  * Plugin Name: Számlahegy Woocommerce
  * Plugin URI:  https://szamlahegy.hu
  * Description: Woocommerce összekapcsolása Számlaheggyel
- * Version:     1.0
+ * Version:     1.1.2
  * Author:      Számlahegy Kft
  * Author URI:  https://github.com/Szamlahegy
  */
