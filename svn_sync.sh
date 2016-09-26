9c05dd5be7c44f10bc1a459ff0f7d3f275ccc441
kepes.peter <kepes.peter@codeplay.hu>
2016-09-26 14:06:00 +0200

    add svn sync

--- svn_sync.sh
+++ svn_sync.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+PLUGIN_VERSION="1.1.2"
+API_VERSION="3.0.1"
+
+rm -rf tmp/szamlahegy-woocommerce-git tmp/szamlahegy-woocommerce-svn
 mkdir tmp/szamlahegy-woocommerce-git
 svn co https://plugins.svn.wordpress.org/szamlahegy-woocommerce/ tmp/szamlahegy-woocommerce-svn
-git archive master | tar -x -C tmp/szamlahegy-woocommerce-git/
+git archive v$PLUGIN_VERSION | tar -x -C tmp/szamlahegy-woocommerce-git/
 cd api
-git archive master | tar -x -C ../tmp/szamlahegy-woocommerce-git/api
+git archive v$API_VERSION | tar -x -C ../tmp/szamlahegy-woocommerce-git/api
+cd ..
+rm -rf tmp/szamlahegy-woocommerce-git/.git* tmp/szamlahegy-woocommerce-git/api/.git* tmp/szamlahegy-woocommerce-git/svn_sync*
+rm -rf tmp/szamlahegy-woocommerce-svn/trunk/*
+cp -r tmp/szamlahegy-woocommerce-git/* tmp/szamlahegy-woocommerce-svn/trunk/
+cd tmp/szamlahegy-woocommerce-svn
+svn cp trunk tags/$PLUGIN_VERSION
+svn ci -m "new version: $PLUGIN_VERSION"
